e3bb750ba6b2f1e0e182242ffccf9d05e8926857
ZhouYu2003 <2359292349@qq.com>
2020-05-31 03:48:10 -0700

    heart: Update release-keys from ZUI 11.5.247

--- revengeos_heart.mk
+++ revengeos_heart.mk
@@ -17,10 +17,10 @@ PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo L78032
 PRODUCT_NAME := revengeos_heart
 
-BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+BUILD_FINGERPRINT := "Lenovo/heart/heart:10/QKQ1.191014.001/11.5.247_200424:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="heart-user 10 QKQ1.190825.002 11.5.229_200114 release-keys" \
+    PRIVATE_BUILD_DESC="heart-user 10 QKQ1.191014.001 11.5.247_200424 release-keys" \
     PRODUCT_NAME="heart" \
     TARGET_DEVICE="heart"
 
