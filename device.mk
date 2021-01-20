2989bc51f56e0e006a3a11483d8fc32743c1e9a7
Art_Chen <Chenxy0201@qq.com>
2021-01-21 01:10:13 +0800

    Revert "Rebrand to zippo"
    This reverts commit 22a941cf9439851daf769fef92e4113df31c2937.

--- device.mk
+++ device.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/lenovo/zippo/zippo-vendor.mk)
+$(call inherit-product-if-exists, vendor/lenovo/heart/heart-vendor.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
--- lineage_heart.mk
+++ lineage_heart.mk
@@ -4,23 +4,23 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-$(call inherit-product, device/lenovo/zippo/device.mk)
+$(call inherit-product, device/lenovo/heart/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Lenovo
-PRODUCT_DEVICE := zippo
+PRODUCT_DEVICE := heart
 PRODUCT_MANUFACTURER := Lenovo
-PRODUCT_MODEL := Lenovo Z6 Pro
-PRODUCT_NAME := lineage_zippo
+PRODUCT_MODEL := Lenovo Z5 Pro GT
+PRODUCT_NAME := lineage_heart
 
 BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="zippo-user 10 QKQ1.190825.002 11.5.229_200114 release-keys" \
-    PRODUCT_NAME="zippo" \
-    TARGET_DEVICE="zippo"
+    PRIVATE_BUILD_DESC="heart-user 10 QKQ1.190825.002 11.5.229_200114 release-keys" \
+    PRODUCT_NAME="heart" \
+    TARGET_DEVICE="heart"
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
